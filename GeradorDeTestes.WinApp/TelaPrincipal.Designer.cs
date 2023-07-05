f65209c9156b86250cbba4aafb4a67f04ad18932
feWalter <taimisefernanda@gmail.com>
2023-07-05 16:52:50 -0300

    Tela Principal

--- GeradorDeTestes.WinApp/TelaPrincipal.Designer.cs
+++ GeradorDeTestes.WinApp/TelaPrincipal.Designer.cs
@@ -23,17 +23,205 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            menu = new MenuStrip();
+            cadastrosMenuItem = new ToolStripMenuItem();
+            testeMenuItem = new ToolStripMenuItem();
+            materiaMenuItem = new ToolStripMenuItem();
+            questaoMenuItem = new ToolStripMenuItem();
+            disciplinasMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            btnInserir = new ToolStripButton();
+            btnEditar = new ToolStripButton();
+            btnExcluir = new ToolStripButton();
+            panelRegistros = new Panel();
+            statusStrip1 = new StatusStrip();
+            labelRodape = new ToolStripStatusLabel();
+            relatóriosToolStripMenuItem = new ToolStripMenuItem();
+            menu.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            panelRegistros.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menu
+            // 
+            menu.ImageScalingSize = new Size(20, 20);
+            menu.Items.AddRange(new ToolStripItem[] { cadastrosMenuItem, relatóriosToolStripMenuItem });
+            menu.Location = new Point(0, 0);
+            menu.Name = "menu";
+            menu.Padding = new Padding(7, 3, 0, 3);
+            menu.Size = new Size(647, 30);
+            menu.TabIndex = 1;
+            menu.Text = "menuStrip1";
+            // 
+            // cadastrosMenuItem
+            // 
+            cadastrosMenuItem.DropDownItems.AddRange(new ToolStripItem[] { testeMenuItem, materiaMenuItem, questaoMenuItem, disciplinasMenuItem });
+            cadastrosMenuItem.Name = "cadastrosMenuItem";
+            cadastrosMenuItem.Size = new Size(88, 24);
+            cadastrosMenuItem.Text = "Cadastros";
+            // 
+            // testeMenuItem
+            // 
+            testeMenuItem.Name = "testeMenuItem";
+            testeMenuItem.ShortcutKeys = Keys.F1;
+            testeMenuItem.Size = new Size(224, 26);
+            testeMenuItem.Text = "Teste";
+            testeMenuItem.Click += testeMenuItem_Click;
+            // 
+            // materiaMenuItem
+            // 
+            materiaMenuItem.Name = "materiaMenuItem";
+            materiaMenuItem.ShortcutKeys = Keys.F2;
+            materiaMenuItem.Size = new Size(224, 26);
+            materiaMenuItem.Text = "Matéria";
+            materiaMenuItem.Click += materiaMenuItem_Click;
+            // 
+            // questaoMenuItem
+            // 
+            questaoMenuItem.Name = "questaoMenuItem";
+            questaoMenuItem.ShortcutKeys = Keys.F3;
+            questaoMenuItem.Size = new Size(224, 26);
+            questaoMenuItem.Text = "Questão";
+            questaoMenuItem.Click += questaoMenuItem_Click;
+            // 
+            // disciplinasMenuItem
+            // 
+            disciplinasMenuItem.Name = "disciplinasMenuItem";
+            disciplinasMenuItem.ShortcutKeys = Keys.F4;
+            disciplinasMenuItem.Size = new Size(224, 26);
+            disciplinasMenuItem.Text = "Disciplina";
+            disciplinasMenuItem.Click += disciplinasMenuItem_Click;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.AutoSize = false;
+            toolStrip1.Enabled = false;
+            toolStrip1.ImageScalingSize = new Size(50, 50);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnInserir, btnEditar, btnExcluir });
+            toolStrip1.LayoutStyle = ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            toolStrip1.Location = new Point(0, 30);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(647, 47);
+            toolStrip1.Stretch = true;
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // btnInserir
+            // 
+            btnInserir.AutoSize = false;
+            btnInserir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnInserir.Image = Properties.Resources.button_iinserir;
+            btnInserir.ImageTransparentColor = Color.Magenta;
+            btnInserir.Margin = new Padding(10, 1, 10, 2);
+            btnInserir.Name = "btnInserir";
+            btnInserir.Size = new Size(44, 36);
+            btnInserir.Text = "Inserir";
+            btnInserir.Click += btnInserir_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.AutoSize = false;
+            btnEditar.BackColor = SystemColors.Control;
+            btnEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnEditar.Image = (Image)resources.GetObject("btnEditar.Image");
+            btnEditar.ImageTransparentColor = Color.Magenta;
+            btnEditar.Margin = new Padding(10, 1, 10, 2);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(40, 34);
+            btnEditar.Text = "Editar";
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.AutoSize = false;
+            btnExcluir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnExcluir.Image = Properties.Resources.button_excluir;
+            btnExcluir.ImageTransparentColor = Color.Magenta;
+            btnExcluir.Margin = new Padding(10, 1, 10, 2);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(35, 30);
+            btnExcluir.Text = "Excluir";
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // panelRegistros
+            // 
+            panelRegistros.Controls.Add(statusStrip1);
+            panelRegistros.Dock = DockStyle.Fill;
+            panelRegistros.Location = new Point(0, 77);
+            panelRegistros.Margin = new Padding(3, 4, 3, 4);
+            panelRegistros.Name = "panelRegistros";
+            panelRegistros.Size = new Size(647, 306);
+            panelRegistros.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { labelRodape });
+            statusStrip1.Location = new Point(0, 280);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Padding = new Padding(1, 0, 16, 0);
+            statusStrip1.Size = new Size(647, 26);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // labelRodape
+            // 
+            labelRodape.Name = "labelRodape";
+            labelRodape.Size = new Size(67, 20);
+            labelRodape.Text = "[rodapé]";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            relatóriosToolStripMenuItem.Size = new Size(90, 24);
+            relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // TelaPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(647, 383);
+            Controls.Add(panelRegistros);
+            Controls.Add(toolStrip1);
+            Controls.Add(menu);
+            Name = "TelaPrincipal";
+            ShowIcon = false;
+            Text = "Gerador de Testes";
+            menu.ResumeLayout(false);
+            menu.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            panelRegistros.ResumeLayout(false);
+            panelRegistros.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem materiaMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem questaoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disciplinasMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnInserir;
+        private System.Windows.Forms.ToolStripButton btnEditar;
+        private System.Windows.Forms.ToolStripButton btnExcluir;
+        private System.Windows.Forms.Panel panelRegistros;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelRodape;
+        private ToolStripMenuItem relatóriosToolStripMenuItem;
     }
 }
--- GeradorDeTestes.WinApp/TelaPrincipal.cs
+++ GeradorDeTestes.WinApp/TelaPrincipal.cs
@@ -1,10 +1,149 @@
+using System.Windows.Forms;
+
 namespace GeradorDeTestes.WinApp
 {
     public partial class TelaPrincipal : Form
     {
-        public TelaPrincipal()
+        private ControladorBase controlador;
+        private Dictionary<string, ControladorBase> controladores;
+        private DataContext contextoDados;
+
+
+        public TelaPrincipal(DataContext contextoDados)
         {
             InitializeComponent();
+            Instancia = this;
+
+            labelRodape.Text = string.Empty;
+            labelTipoCadastro.Text = string.Empty;
+
+            this.contextoDados = contextoDados;
+
+            InicializarControladores();
         }
+        public static TelaPrincipal Instancia
+        {
+            get;
+            private set;
+        }
+        public void AtualizarRodape(string mensagem)
+        {
+            labelRodape.Text = mensagem;
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            controlador.Inserir();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            controlador.Editar();
+        }
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            controlador.Excluir();
+        }
+
+        private void disciplinasMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void materiaMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Disciplina> disciplinas = repositorioDisciplina.SelecionarTodos();
+
+            if (disciplinas.Count > 0)
+            {
+                ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+            }
+            else
+            {
+                AtualizarRodape("Crie uma Disciplina Primeiro");
+            }
+
+        }
+
+        private void questaoMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void testeMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender); ;
+        }
+        private void ConfigurarBotoes(ConfiguracaoToolBoxBase configuracao)
+        {
+            btnInserir.Enabled = configuracao.InserirHabilitado;
+            btnEditar.Enabled = configuracao.EditarHabilitado;
+            btnExcluir.Enabled = configuracao.ExcluirHabilitado;
+
+        }
+
+        private void ConfigurarTooltips(ConfiguracaoToolBoxBase configuracao)
+        {
+            btnInserir.ToolTipText = configuracao.TooltipInserir;
+            btnEditar.ToolTipText = configuracao.TooltipEditar;
+            btnExcluir.ToolTipText = configuracao.TooltipExcluir;
+
+        }
+
+        private void ConfigurarTelaPrincipal(ToolStripMenuItem opcaoSelecionada)
+        {
+            var tipo = opcaoSelecionada.Text;
+
+            controlador = controladores[tipo];
+
+            ConfigurarToolbox();
+
+            ConfigurarListagem();
+        }
+
+        private void ConfigurarToolbox()
+        {
+            ConfiguracaoToolBoxBase configuracao = controlador.ObtemConfiguracaoToolbox();
+
+            if (configuracao != null)
+            {
+                toolStrip1.Enabled = true;
+
+                labelTipoCadastro.Text = configuracao.TipoCadastro;
+
+                ConfigurarTooltips(configuracao);
+
+                ConfigurarBotoes(configuracao);
+            }
+        }
+
+        private void ConfigurarListagem()
+        {
+            AtualizarRodape("");
+
+            var listagemControl = controlador.ObtemListagem();
+
+            panelRegistros.Controls.Clear();
+
+            listagemControl.Dock = DockStyle.Fill;
+
+            panelRegistros.Controls.Add(listagemControl);
+        }
+
+        private void InicializarControladores()
+        {
+            repositorioDisciplina = new RepositorioDisciplinaEmBancoDeDados();
+            repositorioMateria = new RepositorioMateriaEmBancoDeDados();
+            repositorioQuestao = new RepositorioQuestaoEmBancoDeDados();
+            repositorioTeste = new RepositorioTesteEmBancoDeDados();
+
+            controladores = new Dictionary<string, ControladorBase>();
+
+            controladores.Add("Disciplina", new ControladorDisciplina(repositorioDisciplina, repositorioMateria));
+            controladores.Add("Matéria", new ControladorMateria(repositorioMateria, repositorioDisciplina));
+            controladores.Add("Questão", new ControladorQuestao(repositorioDisciplina, repositorioMateria, repositorioQuestao));
+            controladores.Add("Teste", new ControladorTeste(repositorioTeste, repositorioMateria, repositorioDisciplina, repositorioQuestao));
+        }
+
     }
 }
