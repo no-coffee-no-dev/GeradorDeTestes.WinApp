c72652f0e4712ab3ea125c5a96c8c9c037373947
feWalter <taimisefernanda@gmail.com>
2023-07-10 15:25:37 -0300

    Adicionar mapeador disciplina.

---
+++ GeradorDeTestes.Infra.Dados.Sql/ModuloDisciplina/MapeadorDisciplina.cs
@@ -0,0 +1,25 @@
+﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using Microsoft.Data.SqlClient;
+
+namespace GeradorDeTestes.Infra.Dados.Sql.ModuloDisciplina
+{
+    public class MapeadorDisciplina : MapeadorBase<Disciplina>
+    {
+        public override void ConfigurarParametros(SqlCommand comandoEditar, Disciplina novaEntidade)
+        {
+            comandoEditar.Parameters.AddWithValue("ID", novaEntidade.id);
+            comandoEditar.Parameters.AddWithValue("NOME", novaEntidade.nome);
+        }
+
+        public override Disciplina ConverterParaEntidade(SqlDataReader leitorEntidades)
+        {
+            int idDisciplina = Convert.ToInt32(leitorEntidades["ID"]);
+            string nome = Convert.ToString(leitorEntidades["NOME"]);
+
+            Disciplina disciplina = new (nome);
+            disciplina.id = idDisciplina;
+            return disciplina;
+
+        }
+    }
+}
